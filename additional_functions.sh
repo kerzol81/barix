777ccbde3ed81abcbf03fd736b29afda3aef556e
Z <kerenyi.nagy.zoltan@gmail.com>
2019-02-08 21:27:59 +0100

    Update additional_functions.sh
    Additional functions not implemented on the OEM board for the web interface's cgi scripts

--- additional_functions.sh
+++ additional_functions.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -x
-config_file='/etc/audio.conf'
 
-function print_streaming_url
-{
-  if [ ! -e $config_file ];then
-	  echo "no config file"
-	
-else
-	port=$(grep 'HTTPPort' $config_file | awk '{ print $2 }')
-	url_end=$(grep '<Feed' /etc/audio.conf | awk '{ print $2 }' | awk -F '.ffm' '{ print $1 }')
-	ip='10.0.0.10' --> implemented function on device
-	echo "http://$ip:$port/$url_end"
-fi
+function print_streaming_url{	
+	local config_file='/etc/ffserver.conf'
+	if [ ! -e $config_file ];then
+		echo "no config file"
+	else
+		local port=$(grep 'HTTPPort' $config_file | awk '{ print $2 }')
+		local url_end=$(grep '<Feed' /etc/audio.conf | awk '{ print $2 }' | awk -F '.ffm' '{ print $1 }')
+		local ip='_' #already implemented function on device
+		echo "http://$ip:$port/$url_end"
+	fi
 }
