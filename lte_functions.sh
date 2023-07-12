f768d868eba27857761acc8213f8006f75d65bf1
Z <kerenyi.nagy.zoltan@gmail.com>
2023-07-12 07:53:01 +0200

    Update lte_functions.sh

--- lte_functions.sh
+++ lte_functions.sh
@@ -4,3 +4,4 @@ sim_card_state=$(qmicli --device=/dev/cdc-wdm0 --device-open-proxy --uim-get-car
 
 qmicli -d /dev/cdc-wdm0 --device-open-proxy --nas-get-signal-info
 
+connection_status=$(qmicli --wds-get-packet-service-status -d /dev/cdc-wdm0 | grep 'Connection status' | awk '{print $4}' | sed "s/'//g")
