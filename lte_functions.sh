b9ca7ddc7b8459262a8d7efa7212c5b1ec6f5401
Z <kerenyi.nagy.zoltan@gmail.com>
2023-07-12 11:38:02 +0200

    Update lte_functions.sh

--- lte_functions.sh
+++ lte_functions.sh
@@ -7,3 +7,5 @@ qmicli -d /dev/cdc-wdm0 --device-open-proxy --nas-get-signal-info
 connection_status=$(qmicli --wds-get-packet-service-status -d /dev/cdc-wdm0 | grep 'Connection status' | awk '{print $4}' | sed "s/'//g")
 
 imsi=$(qmicli --dms-uim-get-imsi -d /dev/cdc-wdm0 | grep 'IMSI:' | awk '{print $2}' | sed "s/'//g")
+
+qmicli -d /dev/cdc-wdm0 --dms-get-band-capabilities
